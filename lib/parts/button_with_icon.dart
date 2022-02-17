6555fd08e4530dfe571110875e49147b4337dbae
mikippi2426 <mikippi0120@gmail.com>
2022-02-17 14:24:00 +0900

    確認ボタン修正２

--- lib/parts/button_with_icon.dart
+++ lib/parts/button_with_icon.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ButtonWithIcon extends StatelessWidget {
-  final onPressed;
-  final icon;
-  final label;
+  final VoidCallback onPressed;
+  final Icon icon;
+  final String label;
+  final Color color;
 
 
-  ButtonWithIcon({this.onPressed, this.icon,this.label});
+  ButtonWithIcon({
+    required this.onPressed,
+    required this.icon,
+    required this.label,
+    required this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +20,20 @@ class ButtonWithIcon extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 36.0),
       child: SizedBox(
         width: double.infinity,
+        height: 50.0,
         child: ElevatedButton.icon(
-            onPressed: null,
+            style: ElevatedButton.styleFrom(
+              primary: color,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0))
+              )
+            ),
+            onPressed: onPressed,
             icon: icon,
-            label: Text(label)),
+            label: Text(
+                label,
+                style:TextStyle(fontSize:18.0),
+            )),
       ),
     );
   }
