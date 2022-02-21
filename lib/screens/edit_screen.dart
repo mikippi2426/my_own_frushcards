e3d3fdd35576eccccde246fcde21d2ef7a39b0d7
mikippi2426 <mikippi0120@gmail.com>
2022-02-21 13:18:26 +0900

    FloatingActionButton

---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EditScreen extends StatefulWidget {
+  const EditScreen({Key? key}) : super(key: key);
+
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
