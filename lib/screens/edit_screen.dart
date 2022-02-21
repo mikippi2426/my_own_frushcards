0293764f32ec924edf012f18868f750cb93297f5
mikippi2426 <mikippi0120@gmail.com>
2022-02-21 13:41:50 +0900

    問題入力部分

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -8,11 +8,49 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+
+  var questionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(),
+      appBar: AppBar(
+        title: Text("新しい単語の登録"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children:<Widget>[
+          SizedBox(height: 30.0,),
+          Center(
+            child: Text(
+              "問題とこたえを入力して「登録」ボタンを押してください。",
+              style:TextStyle(fontSize: 12.0),
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          //問題入力ボタン
+          _questionIconPart(),
+          //TODO こたえ入力画面
+        ],
+      ),
     );
   }
+
+ Widget _questionIconPart() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: [
+          Text("問題",style:  TextStyle(fontSize: 24.0),),
+          SizedBox(height: 10.0,),
+          TextField(
+            controller: questionController,
+            keyboardType: TextInputType.text,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30.0),
+          )
+        ],
+      ),
+    );
+ }
 }
