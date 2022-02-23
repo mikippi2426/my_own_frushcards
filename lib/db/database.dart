f2e7921b7ed5839f2db7732c3f8c21e946df5c5a
mikippi2426 <mikippi0120@gmail.com>
2022-02-23 12:59:17 +0900

    クエリの作成

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -4,14 +4,15 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+
 part 'database.g.dart';
 
-class Words extends Table{
- TextColumn get strQuestion => text()();
+class Words extends Table {
+  TextColumn get strQuestion => text()();
 
- TextColumn get strAnswer => text()();
+  TextColumn get strAnswer => text()();
 
- @override
+  @override
   // TODO: implement primaryKey
   Set<Column> get primaryKey => {strQuestion};
 }
@@ -21,10 +22,21 @@ class MyDatabase extends _$MyDatabase {
   MyDatabase() : super(_openConnection());
 
   @override
-  // TODO: implement schemaVersion
   int get schemaVersion => 1;
 
+  //Create
+  Future addWord(Word word) => into(words).insert(word);
 
+  //Read
+  Future<List<Word>> get allWords => select(words).get();
+
+  //update
+  Future updateWord(Word word) => update(words).replace(word);
+
+  //delete
+  Future deleteWord(Word word) => (delete(words)
+        ..where((table) => table.strQuestion.equals(word.strQuestion)))
+      .go();
 }
 
 LazyDatabase _openConnection() {
