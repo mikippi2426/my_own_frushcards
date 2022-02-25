187e7a2f7bb904dc93a09c0e9aad5884930b9fcd
mikippi2426 <mikippi0120@gmail.com>
2022-02-25 14:57:53 +0900

    テスト画面の実装

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-enum TestStatus {BEFORE_START, SHOW_QUESTION, SHOW_ANSWER, FINISHED}
+enum TestStatus { BEFORE_START, SHOW_QUESTION, SHOW_ANSWER, FINISHED }
 
 class TestScreen extends StatefulWidget {
   final bool isIncludedMemorizedWords;
-
 
   TestScreen({required this.isIncludedMemorizedWords});
 
@@ -16,9 +14,11 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   int _numberOfQuestion = 0;
 
-  String _textQuestion="テスト";//TODO
+  String _textQuestion = "テスト"; //TODO
 
-  String _textAnswer="こたえ";//TODO
+  String _textAnswer = "こたえ";
+
+  bool _isMemorized = false; //TODO
 
   @override
   Widget build(BuildContext context) {
@@ -28,57 +28,97 @@ class _TestScreenState extends State<TestScreen> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Fab押せたで～"),//TODO
+        onPressed: () => print("Fab押せたで～"), //TODO
         child: Icon(Icons.skip_next),
         tooltip: "次にすすむ",
       ),
       body: Column(
         children: [
-          SizedBox(height: 10.0,),
+          SizedBox(
+            height: 10.0,
+          ),
           _numberOfQuestionsPart(),
-          SizedBox(height: 20.0,),
+          SizedBox(
+            height: 20.0,
+          ),
           _questionCardPart(),
-          SizedBox(height:10.0,),
+          SizedBox(
+            height: 10.0,
+          ),
           _answerCardPart(),
+          SizedBox(
+            height: 10.0,
+          ),
           _isMemorizedCheckPart(),
         ],
       ),
     );
   }
+
   //TODO 残り問題数表示部分
   Widget _numberOfQuestionsPart() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("のこり問題数",style: TextStyle(fontSize: 14.0),),
-        SizedBox(width: 30.0,),
-        Text(_numberOfQuestion.toString(),style: TextStyle(fontSize: 24.0),),
+        Text(
+          "のこり問題数",
+          style: TextStyle(fontSize: 14.0),
+        ),
+        SizedBox(
+          width: 30.0,
+        ),
+        Text(
+          _numberOfQuestion.toString(),
+          style: TextStyle(fontSize: 24.0),
+        ),
       ],
     );
   }
 
   //問題カード表示部分
- Widget _questionCardPart() {
+  Widget _questionCardPart() {
     return Stack(
       alignment: Alignment.center,
-      children:[
-      Image.asset("assets/images/image_flash_question.png"),
-      Text(_textQuestion,style: TextStyle(color: Colors.grey[800],fontSize: 20.0),)
-   ],
+      children: [
+        Image.asset("assets/images/image_flash_question.png"),
+        Text(
+          _textQuestion,
+          style: TextStyle(color: Colors.grey[800], fontSize: 20.0),
+        )
+      ],
     );
   }
+
   //TODO こたえカード
   Widget _answerCardPart() {
     return Stack(
       alignment: Alignment.center,
       children: [
         Image.asset("assets/images/image_flash_answer.png"),
-        Text(_textAnswer,style: TextStyle(fontSize: 20.0),),
+        Text(
+          _textAnswer,
+          style: TextStyle(fontSize: 20.0),
+        ),
       ],
     );
   }
+
   //TODO 暗記済みチェック部分
   Widget _isMemorizedCheckPart() {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+      child: CheckboxListTile(
+        title: Text(
+          "暗記済にする場合はチェックを入れて下さい",
+          style: TextStyle(fontSize: 12.0),
+        ),
+        value: _isMemorized,
+        onChanged: (value) {
+          setState(() {
+            _isMemorized = value!;
+          });
+        },
+      ),
+    );
   }
 }
