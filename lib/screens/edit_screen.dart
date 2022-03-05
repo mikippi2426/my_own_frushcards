bc590bef8588d5d9ca271abd04b50ecfdad79b1c
mikippi2426 <mikippi0120@gmail.com>
2022-03-05 13:28:50 +0900

    WillPopScope入れた

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_own_frushcards/screens/world_list_screen.dart';
 
 class EditScreen extends StatefulWidget {
   const EditScreen({Key? key}) : super(key: key);
@@ -14,39 +15,42 @@ class _EditScreenState extends State<EditScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("新しい単語の登録"),
-        centerTitle: true,
-      ),
-      body:SingleChildScrollView(
-        child: Column(
-          children:<Widget>[
-            SizedBox(height: 30.0,),
-            Center(
-              child: Text(
-                "問題とこたえを入力して「登録」ボタンを押してください。",
-                style:TextStyle(fontSize: 12.0),
+    return WillPopScope(
+      onWillPop: () => _backToWordListScreen(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("新しい単語の登録"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30.0,),
+              Center(
+                child: Text(
+                  "問題とこたえを入力して「登録」ボタンを押してください。",
+                  style: TextStyle(fontSize: 12.0),
+                ),
               ),
-            ),
-            SizedBox(height: 30.0,),
-            //問題入力部分
-            _questionIconPart(),
-            SizedBox(height: 50.0,),
-            //こたえ入力部分
-            _answerInputPart(),
-          ],
+              SizedBox(height: 30.0,),
+              //問題入力部分
+              _questionIconPart(),
+              SizedBox(height: 50.0,),
+              //こたえ入力部分
+              _answerInputPart(),
+            ],
+          ),
         ),
       ),
     );
   }
 
- Widget _questionIconPart() {
+  Widget _questionIconPart() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
-          Text("問題",style:  TextStyle(fontSize: 24.0),),
+          Text("問題", style: TextStyle(fontSize: 24.0),),
           SizedBox(height: 10.0,),
           TextField(
             controller: questionController,
@@ -57,14 +61,14 @@ class _EditScreenState extends State<EditScreen> {
         ],
       ),
     );
- }
+  }
 
-  Widget  _answerInputPart() {
+  Widget _answerInputPart() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
-          Text("こたえ",style:  TextStyle(fontSize: 24.0),),
+          Text("こたえ", style: TextStyle(fontSize: 24.0),),
           SizedBox(height: 10.0,),
           TextField(
             controller: answerController,
@@ -75,5 +79,11 @@ class _EditScreenState extends State<EditScreen> {
         ],
       ),
     );
+  }
+
+  Future<bool> _backToWordListScreen() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => WordListScreen()));
+    return Future.value(true);
   }
 }
